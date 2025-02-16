1b312ffde0f5476b48d8d88b2abc3575c3bac55a
KyotakaMD45 <daemonabraxas08@gmail.com>
2025-02-16 13:19:47 +0000

    Update gc-mute.js

--- Jarsekai/gc-mute.js
+++ Jarsekai/gc-mute.js
@@ -1,11 +1,11 @@
 let jarsepay = async (m, { conn, usedPrefix }) => {
 	let chat = global.db.data.chats[m.chat]
 	if (chat.isBanned === true) {
-		m.reply('Chat ini sudah dalam keadaan mute.')
+		m.reply('Ce chat est désormais désactivé.')
 		return
 	}
 	chat.isBanned = true
-	await m.reply('Bot berhasil di mute pada chat ini.')
+	await m.reply('Le bot a été désactivé avec succès dans ce chat.')
 }
 jarsepay.help = ['mute']
 jarsepay.tags = ['group']
